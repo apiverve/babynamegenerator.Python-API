383f058f0e211a5cc6d75dae5291397cf9402251
APIVerve Team <hello@apiverve.com>
2025-03-28 17:00:44 -0500

    Bug Fixes

--- src/apiverve_babynamegenerator/apiClient.py
+++ src/apiverve_babynamegenerator/apiClient.py
@@ -10,10 +10,10 @@ class BabynamegeneratorAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/babynamegenerator'
+        self.base_url = 'https://api.apiverve.com/v1/babynamegenerator'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
